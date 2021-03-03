d3fe9c027419e1955063a65585a6ce5104936e23
mohitg85 <mohitg85@gmail.com>
2021-03-03 04:15:58 +0000

    SAST Fixed

--- src/main/java/com/notsosecure/devsecops/actions/api/StudentAddApi.java
+++ src/main/java/com/notsosecure/devsecops/actions/api/StudentAddApi.java
@@ -26,43 +26,31 @@ public class StudentAddApi extends ActionSupport implements ServletResponseAware
 
 //	VULNERABLE CODE
 //	@Action("/api/add")
-	public String execute() throws Exception {
-		System.out.println("Inside Student Add");
-		StudentService studentService = new StudentService();
-		xmlString = servletRequest.getInputStream();
-		XMLDecoder xmlDecoder = new XMLDecoder(xmlString);
-		Student student = (Student) xmlDecoder.readObject();
+    public String execute() throws Exception {
+        System.out.println("Inside Student Add");
+        StudentService studentService = new StudentService();
+        xmlString = servletRequest.getInputStream();
+        XMLDecoder xmlDecoder = new XMLDecoder(xmlString);
+        Student student = (Student) xmlDecoder.readObject();
 //		Student student = new Student("test","test","test","test","test","test@test.com");
 //		System.out.println(student.getFirstName());
-		String result;
-		result=studentService.save(student.getUserName(), student.getPassword(), student.getFirstName(), student.getLastName(),
-				dateFormat.format(student.getDateOfBirth()), student.getEmailAddress());
-		PrintWriter printWriter = servletResponse.getWriter();
-		switch (result) {
-		case "SignupFailure-UserNameExists":
-			printWriter.println("Username Exists");
-			break;
-		case "SignupSuccess":
-			printWriter.println("Users Added Successfully");
-			break;
-		case "SignupFailure":
-			printWriter.println("Something Went wrong");
-			break;
-		default:
-			break;
-		}
+        String result;
+        result=studentService.save(student.getUserName(), student.getPassword(), student.getFirstName(), student.getLastName(),
+                dateFormat.format(student.getDateOfBirth()), student.getEmailAddress());
+        PrintWriter printWriter = servletResponse.getWriter();
+        switch (result) {
+        case "SignupFailure-UserNameExists":
+            printWriter.println("Username Exists");
+            break;
+        case "SignupSuccess":
+            printWriter.println("Users Added Successfully");
+            break;
+        case "SignupFailure":
+            printWriter.println("Something Went wrong");
+            break;
+        default:
+            break;
+        }
 
-		return NONE;
-	}
-
-	@Override
-	public void setServletRequest(HttpServletRequest request) {
-		this.servletRequest = request;
-	}
-
-	@Override
-	public void setServletResponse(HttpServletResponse response) {
-		this.servletResponse = response;
-	}
-
-}
+        return NONE;
+    }
